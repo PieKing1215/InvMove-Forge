2b18f6d2dc5a2034fef1e6627cf1891354dce78d
David M <PieKing1215@users.noreply.github.com>
2020-06-21 01:23:41 -0400

    Make config background transparent if possible
    When opened from ingame, the config background will be transparent.

--- src/main/java/me/pieking1215/invmove/Config.java
+++ src/main/java/me/pieking1215/invmove/Config.java
@@ -251,6 +251,7 @@ public class Config {
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (client, parent) -> {
             ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle("config.invmove.title");
             builder.setDefaultBackgroundTexture(new ResourceLocation("minecraft:textures/block/spruce_planks.png"));
+            builder.transparentBackground();
 
             ConfigEntryBuilder eb = builder.getEntryBuilder();
             ConfigCategory general = builder.getOrCreateCategory("key.invmove.category.general");
