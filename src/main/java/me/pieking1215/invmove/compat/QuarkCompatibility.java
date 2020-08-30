7b769c5e8ae2610f639bea05c82e0ef353154f4a
David M <PieKing1215@users.noreply.github.com>
2020-08-30 13:49:26 -0400

    Initial 1.16.1 port

--- src/main/java/me/pieking1215/invmove/compat/QuarkCompatibility.java
+++ src/main/java/me/pieking1215/invmove/compat/QuarkCompatibility.java
@@ -1,56 +1,56 @@
-//package me.pieking1215.invmove.compat;
-//
-//import me.pieking1215.invmove.Config;
-//import net.minecraft.client.gui.screen.Screen;
-//import net.minecraft.client.gui.widget.TextFieldWidget;
-//import vazkii.quark.client.module.ChestSearchingModule;
-//
-//import java.lang.reflect.Field;
-//import java.util.Arrays;
-//import java.util.Optional;
-//import java.util.concurrent.atomic.AtomicBoolean;
-//
-//public class QuarkCompatibility extends ModCompatibility {
-//
-//    Field ChestSearchingModule_searchBar = null;
-//
-//    AtomicBoolean SPECIAL_ChestSearchFocus_movement = new AtomicBoolean(false);
-//
-//    public QuarkCompatibility(){
-//        movementOptions = Arrays.asList(
-//                new BoolOption("When Chest Search Focused", "SPECIAL_ChestSearchFocus_movement", SPECIAL_ChestSearchFocus_movement,false)
-//        );
-//        backgroundOptions = Arrays.asList(
-//        );
-//    }
-//
-//    @Override
-//    public Optional<Boolean> shouldAllowMovement(Screen screen) {
-//
-//        if(!SPECIAL_ChestSearchFocus_movement.get() && Config.getBoolSafe(Config.GENERAL.textFieldDisablesMovement, true)) {
-//            if (ChestSearchingModule_searchBar == null) {
-//                Field[] fs = ChestSearchingModule.class.getDeclaredFields();
-//                for (Field f : fs) {
-//                    if (f.getType() == TextFieldWidget.class) {
-//                        ChestSearchingModule_searchBar = f;
-//                        ChestSearchingModule_searchBar.setAccessible(true);
-//                    }
-//                }
-//            } else {
-//                try {
-//                    TextFieldWidget tw = (TextFieldWidget) ChestSearchingModule_searchBar.get(null);
-//                    if (tw != null && tw.canWrite()) return Optional.of(false);
-//                } catch (IllegalAccessException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//
-//        return Optional.empty();
-//    }
-//
-//    @Override
-//    public Optional<Boolean> shouldDisableBackground(Screen screen) {
-//        return Optional.empty();
-//    }
-//}
+package me.pieking1215.invmove.compat;
+
+import me.pieking1215.invmove.Config;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import vazkii.quark.client.module.ChestSearchingModule;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class QuarkCompatibility extends ModCompatibility {
+
+    Field ChestSearchingModule_searchBar = null;
+
+    AtomicBoolean SPECIAL_ChestSearchFocus_movement = new AtomicBoolean(false);
+
+    public QuarkCompatibility(){
+        movementOptions = Arrays.asList(
+                new BoolOption("When Chest Search Focused", "SPECIAL_ChestSearchFocus_movement", SPECIAL_ChestSearchFocus_movement,false)
+        );
+        backgroundOptions = Arrays.asList(
+        );
+    }
+
+    @Override
+    public Optional<Boolean> shouldAllowMovement(Screen screen) {
+
+        if(!SPECIAL_ChestSearchFocus_movement.get() && Config.getBoolSafe(Config.GENERAL.textFieldDisablesMovement, true)) {
+            if (ChestSearchingModule_searchBar == null) {
+                Field[] fs = ChestSearchingModule.class.getDeclaredFields();
+                for (Field f : fs) {
+                    if (f.getType() == TextFieldWidget.class) {
+                        ChestSearchingModule_searchBar = f;
+                        ChestSearchingModule_searchBar.setAccessible(true);
+                    }
+                }
+            } else {
+                try {
+                    TextFieldWidget tw = (TextFieldWidget) ChestSearchingModule_searchBar.get(null);
+                    if (tw != null && tw.canWrite()) return Optional.of(false);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Boolean> shouldDisableBackground(Screen screen) {
+        return Optional.empty();
+    }
+}
