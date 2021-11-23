f125a6594da8aec23fb1c9b226bc9019bb1295b9
David M <PieKing1215@users.noreply.github.com>
2021-11-23 13:26:26 -0500

    Reimplement Charm compat

--- src/main/java/me/pieking1215/invmove/compat/CharmCompatibility.java
+++ src/main/java/me/pieking1215/invmove/compat/CharmCompatibility.java
@@ -1,47 +1,55 @@
-//package me.pieking1215.invmove.compat;
-//
-//import net.minecraft.client.gui.screen.Screen;
-//import svenhjol.charm.decoration.inventory.BookshelfChestScreen;
-//import svenhjol.charm.decoration.inventory.CrateScreen;
-//
-//import java.util.Arrays;
-//import java.util.Optional;
-//import java.util.concurrent.atomic.AtomicBoolean;
-//
-//public class CharmCompatibility extends ModCompatibility {
-//
-//    AtomicBoolean BookshelfChestScreen_movement  = new AtomicBoolean(true);
-//    AtomicBoolean CrateScreen_movement  = new AtomicBoolean(true);
-//    AtomicBoolean BookshelfChestScreen_background_disable  = new AtomicBoolean(true);
-//    AtomicBoolean CrateScreen_background_disable  = new AtomicBoolean(true);
-//
-//    public CharmCompatibility(){
-//        movementOptions = Arrays.asList(
-//                new BoolOption("Bookshelf Chest", "BookshelfChestScreen_movement", BookshelfChestScreen_movement, true),
-//                new BoolOption("Crate", "CrateScreen_movement", CrateScreen_movement,true)
-//        );
-//        backgroundOptions = Arrays.asList(
-//                new BoolOption("Bookshelf Chest", "BookshelfChestScreen_background_disable", BookshelfChestScreen_background_disable, true),
-//                new BoolOption("Crate", "CrateScreen_background_disable", CrateScreen_background_disable,true)
-//        );
-//    }
-//
-//    @Override
-//    public Optional<Boolean> shouldAllowMovement(Screen screen) {
-//
-//        if(screen instanceof BookshelfChestScreen) return Optional.of(BookshelfChestScreen_movement.get());
-//        if(screen instanceof CrateScreen)          return Optional.of(CrateScreen_movement.get());
-//
-//        return Optional.empty();
-//    }
-//
-//    @Override
-//    public Optional<Boolean> shouldDisableBackground(Screen screen) {
-//
-//        if(screen instanceof BookshelfChestScreen) return Optional.of(BookshelfChestScreen_background_disable.get());
-//        if(screen instanceof CrateScreen)          return Optional.of(CrateScreen_background_disable.get());
-//
-//        return Optional.empty();
-//    }
-//
-//}
+package me.pieking1215.invmove.compat;
+
+import net.minecraft.client.gui.screen.Screen;
+import svenhjol.charm.base.gui.CharmContainerScreen;
+import svenhjol.charm.gui.KilnScreen;
+import svenhjol.charm.gui.WoodcutterScreen;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class CharmCompatibility extends ModCompatibility {
+
+    AtomicBoolean CharmContainerScreen_movement  = new AtomicBoolean(true);
+    AtomicBoolean Kiln_movement  = new AtomicBoolean(true);
+    AtomicBoolean Woodcutter_movement  = new AtomicBoolean(true);
+    AtomicBoolean CharmContainerScreen_background_disable  = new AtomicBoolean(true);
+    AtomicBoolean Kiln_background_disable  = new AtomicBoolean(true);
+    AtomicBoolean Woodcutter_background_disable  = new AtomicBoolean(true);
+
+    public CharmCompatibility(){
+        movementOptions = Arrays.asList(
+                new BoolOption("Bookshelf/Crate", "CharmContainerScreen_movement", CharmContainerScreen_movement, true),
+                new BoolOption("Kiln", "Kiln_movement", Kiln_movement,true),
+                new BoolOption("Woodcutter", "Woodcutter_movement", Woodcutter_movement,true)
+        );
+        backgroundOptions = Arrays.asList(
+                new BoolOption("Bookshelf/Crate", "CharmContainerScreen_background_disable", CharmContainerScreen_background_disable, true),
+                new BoolOption("Kiln", "Kiln_background_disable", Kiln_background_disable,true),
+                new BoolOption("Woodcutter", "Woodcutter_background_disable", Woodcutter_background_disable,true)
+        );
+    }
+
+    @Override
+    public Optional<Boolean> shouldAllowMovement(Screen screen) {
+
+        // bookshelf and crate
+        if(screen instanceof CharmContainerScreen) return Optional.of(CharmContainerScreen_movement.get());
+        if(screen instanceof KilnScreen) return Optional.of(Kiln_movement.get());
+        if(screen instanceof WoodcutterScreen) return Optional.of(Woodcutter_movement.get());
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Boolean> shouldDisableBackground(Screen screen) {
+
+        if(screen instanceof CharmContainerScreen) return Optional.of(CharmContainerScreen_background_disable.get());
+        if(screen instanceof KilnScreen) return Optional.of(Kiln_background_disable.get());
+        if(screen instanceof WoodcutterScreen) return Optional.of(Woodcutter_background_disable.get());
+
+        return Optional.empty();
+    }
+
+}
